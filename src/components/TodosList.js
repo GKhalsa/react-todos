612e7a9636abbe1147797921ad0a4d84cf480ebe
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-09-07 18:17:29 -0600

    add todoslistitem component

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,12 +1,23 @@
+// import _ from 'lodash';
 import React from 'react';
 import TodosListHeader from './TodosListHeader';
+import TodosListItem from './TodosListItem';
 
 export default class TodosList extends React.Component {
+
+  renderItems(todo, index) {
+    return <TodosListItem key={index} {...todo}/>
+  }
+
   render(){
     return (
       <table>
         <TodosListHeader/>
+        <tbody>
+          {this.props.todos.map(this.renderItems)}
+        </tbody>
       </table>
     );
   }
+
 }
